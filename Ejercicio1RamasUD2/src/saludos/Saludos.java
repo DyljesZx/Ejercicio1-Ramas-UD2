e032795db1228c8d3060f3e7d134ef6b6bb4cb9d
Dylan Flores <24.daw.dylanflores@fundacionmontessori.com>
2024-11-22 17:04:41 +0100

    saludosNombre

--- Ejercicio1RamasUD2/src/saludos/Saludos.java
+++ Ejercicio1RamasUD2/src/saludos/Saludos.java
@@ -1,10 +1,22 @@
 package saludos;
 
+import java.util.Scanner;
+
 public class Saludos {
 
 	public static void main(String[] args) {
-
-		System.out.println("Hola mundo");
+		Scanner input = new Scanner (System.in);
+		
+		System.out.println("Su nombre es: ");
+		String nombre = input.nextLine();
+	
+		System.out.println("Su apellido es: ");
+		String apellido = input.nextLine();
+		
+		System.out.println("Hola " + nombre + " " + apellido);
+		
+		
+		input.close();
 	}
 
 }
